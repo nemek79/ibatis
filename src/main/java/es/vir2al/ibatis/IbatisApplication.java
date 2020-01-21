9df818e0025a2706d87815591656b526c53cecb1
git <davidalonso79@gmail.com>
2020-01-21 19:23:04 +0100

    Inicio repo

---
+++ src/main/java/es/vir2al/ibatis/IbatisApplication.java
@@ -0,0 +1,13 @@
+package es.vir2al.ibatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/vir2al/ibatis/IbatisApplicationTests.java
@@ -0,0 +1,13 @@
+package es.vir2al.ibatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
